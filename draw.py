87ae285e77a5a1aa77160cb5d7479758962fb27b
dev-nino-sarkar <rishav_25cse200@gkciet.ac.in>
2026-09-17 00:41:00 +0530

    draw.py

---
+++ draw.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((500, 500, 3), dtype = 'uint8')
+cv.imshow('Blank', blank)
+img = cv.imread('image.jpeg')
+cv.imshow('Image', img)
+
+# 1. point the image a certain colour
+blank[:] = 0, 255, 0
+cv.imshow('Green', blank)
+
+# 2. draw a rectangle
+cv.rectangle(blank, (0, 0), (250, 500), (0, 0, 255), thickness=2)
+cv.imshow('Rectangle', blank)
+
+# 3. draw a circle
+cv.circle(blank, (blank.shape[1] // 2, blank.shape[0] // 2), 40, (0, 0, 255), thickness=1)
+cv.imshow('Circle', blank)
+
+# 4. draw a line
+cv. line(blank, (0, 0), (blank.shape[1] // 2, blank.shape[0] // 2), (0, 255, 0), thickness = 1)
+cv.imshow('Line', blank)
+
+# 5. write a text
+cv.putText(blank, 'Hello', (225, 225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0, 255, 0), 2)
+cv.imshow('Text', blank)
+
+cv.waitKey(0)
